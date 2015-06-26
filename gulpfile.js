f4547fc652a42c08099d1409bdec9a897e603617
nealfennimore <hi@neal.codes>
2015-06-26 14:42:05 -0700

    Remove gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 
@@ -14,11 +13,10 @@ gulp.task('styles', function(){
         this.emit('end');
     }}))
     .pipe(sass())
-    // .pipe(autoprefixer('last 2 versions'))
-    // .pipe(gulp.dest('css/'))
-    // .pipe(rename({suffix: '.min'}))
-    // .pipe(minifycss())
     .pipe(gulp.dest('css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css/'));
 });
 
 
