ce97c269ff242a75c67e33c0e845c4737d89f8d4
nealfennimore <hi@neal.codes>
2015-06-26 00:08:24 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles', function(){
+  gulp.src(['scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    // .pipe(autoprefixer('last 2 versions'))
+    // .pipe(gulp.dest('css/'))
+    // .pipe(rename({suffix: '.min'}))
+    // .pipe(minifycss())
+    .pipe(gulp.dest('css/'))
+});
+
+
+gulp.task('default', function(){
+  gulp.watch("scss/**/*.scss", ['styles']);
+});
