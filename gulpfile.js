3864c8e13b86b416f0eecd73f573f96838262ec7
nealfennimore <hi@neal.codes>
2015-06-26 15:56:29 -0700

    Add in autoprefixer-core and gulp-postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('autoprefixer-core'),
+    postcss = require('gulp-postcss');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 
@@ -14,7 +15,7 @@ gulp.task('styles', function(){
         this.emit('end');
     }}))
     .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
     .pipe(gulp.dest('css/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
